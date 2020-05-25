bad0f1db58c4c2ff8ac9bd586c466672a67280ee
danheeks <danheeks@gmail.com>
2020-05-25 17:34:24 +0100

    work on rpi build

--- Sim/VoxelCut.cpp
+++ Sim/VoxelCut.cpp
@@ -36,7 +36,6 @@ extern void draw_callback();
 long initapp (long argc, char **argv)
 {
 	char *level = NULL;
-	char *stlfile = NULL;
 	
 	xres = 1024; yres = 768; colbits = 32; fullscreen = 0;
 
@@ -57,51 +56,6 @@ long initapp (long argc, char **argv)
 		pmax.x = 1023; pmax.y = 1023; pmax.z = 1023;
 		setrect(&pmin, &pmax, -1);
 	}
-
-	if(stlfile){
-		ifstream ifs(stlfile, ios::binary);
-		if(!ifs){
-			char mess[1024];
-			sprintf(mess, "couldn't load stl file - %s", stlfile);
-			MessageBox(NULL, Ctt(mess), Ctt("error"), MB_OK);
-			return -1;
-		}
-
-		int type = 0;
-
-		while(1){
-			ifs.read((char *)(&type), sizeof(int));
-			if(type == 1){// triangle
-				double x[9];
-				ifs.read((char *)(x), 9*sizeof(double));
-				point3d p1, p2, p3;
-				p1.x = (float)x[0];
-				p1.y = (float)x[1];
-				p1.z = 256.0f - (float)x[2];
-				p2.x = (float)x[3];
-				p2.y = (float)x[4];
-				p2.z = 256.0f - (float)x[5];
-				p3.x = (float)x[6];
-				p3.y = (float)x[7];
-				p3.z = 256.0f - (float)x[8];
-				settri(&p1, &p2, &p3, 0);
-			}
-			else if(type == 2){// flood fill point
-				double p[3];
-				double e[6];
-				ifs.read((char *)(p), 3*sizeof(double));
-				ifs.read((char *)(e), 6*sizeof(double));
-				setfloodfill3d ((long)p[0], (long)p[1], 256 - (long)p[2], (long)e[0], (long)e[1], 256 - (long)e[5], (long)e[3], (long)e[4], 256 - (long)e[2]);
-				draw_point dp;
-				dp.x = (float)p[0]; dp.y = (float)p[1]; dp.z = 256.0f - (float)p[2];
-				setcube((long)dp.x, (long)dp.y, (long)dp.z, 982374);
-			}
-			else{
-				break;
-			}
-		}
-	}
-
 	updatevxl();
 	
 	vx5.fallcheck = 1;
